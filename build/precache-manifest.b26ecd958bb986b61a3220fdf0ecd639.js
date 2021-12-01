e6bdb437f2943146255af72564671e038052baf7
Emmanuel Akinfenwa <emmanuel@Emmanuels-MBP.attlocal.net>
2021-11-30 22:30:41 -0600

    First Ever Commit

---
+++ build/precache-manifest.b26ecd958bb986b61a3220fdf0ecd639.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cb96870ad9bacd454f46a76d4caa2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6666a50004007521f343",
+    "url": "/static/css/main.d5a0b189.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "6666a50004007521f343",
+    "url": "/static/js/main.8fb01770.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c256e2bbb027178153b5f727bf7ac841",
+    "url": "/static/media/GothamRounded-Bold.c256e2bb.woff"
+  },
+  {
+    "revision": "cf6af960be2a0419fe1c882a8e7e513f",
+    "url": "/static/media/GothamRounded-Bold.cf6af960.woff2"
+  },
+  {
+    "revision": "8e267ddb4734795dcfd59e6749b211b3",
+    "url": "/static/media/GothamRounded-Book.8e267ddb.woff2"
+  },
+  {
+    "revision": "e4d3e8a24917b96ed1521e519712af0e",
+    "url": "/static/media/GothamRounded-Book.e4d3e8a2.woff"
+  },
+  {
+    "revision": "d4f35094a237b47c54e335309b73efd3",
+    "url": "/static/media/GothamRounded-Light.d4f35094.woff2"
+  },
+  {
+    "revision": "f2d6aa9aeea75a50710093c59434472a",
+    "url": "/static/media/GothamRounded-Light.f2d6aa9a.woff"
+  }
+]);
